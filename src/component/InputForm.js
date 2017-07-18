4935e7d956f5ccc25dc68a4f1a76cd57964087c1
nguyenlyminhman <nguyenlyminhman@gmail.com>
2017-07-18 15:39:04 +0700

    finish all

--- src/component/InputForm.js
+++ src/component/InputForm.js
@@ -8,12 +8,14 @@ export default class InputForm extends Component {
     getMessage() {
         const ms = this.refs.txtMS.value;
         this.refs.txtMS.value = '';
+        //phương thức handleReceiveMS(ms) đẩy tin vừa nhận đc sang MainForm
+        //thực chất nó là 1 props trong react
         this.props.handleReceiveMS(ms);
     }
     render() {
         return (
             <div>
-                <input type="text" ref="txtMS" placeholder="message in here" />
+                <input type="text" ref="txtMS" placeholder="Message in here" />
                 <br /> <br />
                 <button onClick={this.getMessage}>Send </button>
             </div>
--- src/component/MainForm.js
+++ src/component/MainForm.js
@@ -3,13 +3,13 @@ import InputForm from './InputForm';
 import Message from './Message';
 
 export default class MainForm extends Component {
-
+    //khởi tạo
     constructor(props) {
         super(props);
-        this.state = { ms: 'hello' };
+        this.state = { ms: 'hello react js' };
         this.onReceiveMS = this.onReceiveMS.bind(this);
     }
-
+    //gán gại giá trị cho state
     onReceiveMS(ms) {
         this.setState({ ms });
     }
@@ -18,7 +18,8 @@ export default class MainForm extends Component {
         return (
             <div>
                 <Message ms={ms} />
-                
+                {/* props handleReceiveMS gọi phương thức onReceiveMS(ms)
+                    và gáng lại giá trị mới cho state "ms" */}
                 <InputForm handleReceiveMS={this.onReceiveMS} />
             </div>
         );
